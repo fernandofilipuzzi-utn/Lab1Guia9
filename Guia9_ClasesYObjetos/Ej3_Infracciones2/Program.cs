eb17be553ec7a5a6b566a6ba4d6ec87d1ac0cc3e
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-05-31 22:15:33 -0300

    Ejercicio 2 - infracciones

---
+++ Guia9_ClasesYObjetos/Ej3_Infracciones2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3_Infracciones2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SistemaInfraccion sistema = new SistemaInfraccion();
+
+            #region pantalla de inicio del sistema
+            Console.Clear();
+            Console.WriteLine(" Iniciando el sistema ");
+            Console.WriteLine(" Ingrese base monetaria (litros/costo litro)");
+            double baseMonetaria = Convert.ToDouble( Console.ReadLine() );
+            sistema.IniciarSistemaInfracciones(baseMonetaria);
+            #endregion
+
+            ConsoleKeyInfo key;
+            do
+            {
+                #region pantalla del menú
+                Console.Clear();
+                Console.WriteLine("1- Ingresar acta.");
+                Console.WriteLine("2- Mostrar recaudación.");
+                Console.WriteLine("Otros- Mostrar recaudación.");
+                key = Console.ReadKey();
+                #endregion
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        {
+                            #region pantalla de ingreso de acta
+                            Console.Clear();
+
+                            Console.WriteLine("Ingreso del acta");
+                            Console.WriteLine("Ingresar dni, nombre, tipo vehiculo (1,2,3) infractor");
+                            int dni = Convert.ToInt32(Console.ReadLine());
+                            string nombre = Console.ReadLine();
+                            int tipoVehiculo = Convert.ToInt32(Console.ReadLine());
+                            sistema.IniciarActa(dni, nombre, tipoVehiculo);
+
+                            Console.WriteLine("{0,10}{1,30}", sistema.dniActa, sistema.nombreActa);
+                            Console.WriteLine("----------------------------------------");
+
+                            Console.WriteLine("Ingresar el tipo de infracción");
+                            int codigo = Convert.ToInt32(Console.ReadLine());
+                            while (codigo > 0)
+                            {
+                                sistema.AgregarInfraccion(codigo);
+                                Console.WriteLine("{0,10}{1,40}({2,10}){3,10:f2}", 
+                                                                                sistema.codigoInfraccion, 
+                                                                                sistema.descripcionInfraccion, 
+                                                                                sistema.unidadesInfraccion,
+                                                                                sistema.montoInfraccion);
+
+                                Console.WriteLine("Ingresar el tipo de infracción");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                            }
+
+                            Console.WriteLine("----------------------------------------");
+                            Console.WriteLine("Paga en el  luegar");
+                            int tipoPago = Convert.ToInt32(Console.ReadLine());
+                            sistema.FinalizarActa(tipoPago == 1);
+                            Console.WriteLine("----------------------------------------");
+                            Console.WriteLine("Subtotal: ${0,10:f2}", sistema.subTotalActa);
+                            Console.WriteLine("Ajuste tipo vehiculo:$-{0,10:f2}", sistema.ajusteTipoVehiculo);
+                            Console.WriteLine("----------------------------------------");
+                            Console.WriteLine("Total a pagar: ${0,10:f2}",sistema.subTotalAjustadoActa);
+                            Console.WriteLine("Descuento Pago en lugar:$ -{0,10:f2}", sistema.descuentoPagoActa);
+                            Console.WriteLine("----------------------------------------");
+                            Console.WriteLine("Total:$ {0,10:f2}", sistema.totalActaAPagar);
+                            Console.WriteLine("----------------------------------------");
+
+                            Console.WriteLine("Presione una tecla para volver al menú");
+                            Console.ReadKey();
+                            #endregion
+                        }
+                        break;                        
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        {
+                            #region pantalla de ingreso de acta
+                            Console.Clear();
+                            
+                            Console.WriteLine("Detalle del final del día");
+                            Console.WriteLine("Recaudación: ${0,10:f2}",sistema.recaudacion);
+
+                            Console.WriteLine("Presione una tecla para volver al menú");
+                            Console.ReadKey();
+                            #endregion
+                        }
+                        break;
+                }
+
+            } while (key.Key != ConsoleKey.D0 && key.Key != ConsoleKey.NumPad0);
+        }
+    }
+}
---
+++ Guia9_ClasesYObjetos/Ej3_Infracciones2/SistemaInfraccion.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej3_Infracciones2
+{
+    /// <summary>
+    /// un acta tiene muchas multas
+    /// </summary>
+    class SistemaInfraccion
+    {
+        #region tabla multas
+        int TIPO_1_MONTO = 25;
+        string TIPO_1_DESCRP = "Sin luces bajas, ley 25….";
+        int TIPO_2_MONTO = 30;
+        string TIPO_2_DESCRP = "Falta de Matafuego, ley 2…";
+        int TIPO_3_MONTO = 100;
+        string TIPO_3_DESCRP = "Sobrevelocidad";
+        int TIPO_4_MONTO = 85;
+        string TIPO_4_DESCRP = "Falta de cinturón de seguridad (>2 ejes) o falta de casco (1 eje)";
+        int TIPO_5_MONTO = 1500;
+        string TIPO_5_DESCRP = "Falta de respeto A la autoridad";
+        #endregion
+
+        #region del sistema de infracciones
+        double baseMonetaria;
+        public double recaudacion;
+        #endregion
+
+        #region por acta
+        public int dniActa;
+        public string nombreActa;
+        int tipoVehiculo;
+        public double subTotalActa;
+        public double ajusteTipoVehiculo;
+        public double subTotalAjustadoActa;
+        public double descuentoPagoActa;
+        public double totalActaAPagar;
+        #endregion
+
+        #region por infraccion
+        public int codigoInfraccion;
+        public string descripcionInfraccion;
+        public int unidadesInfraccion;
+        public double montoInfraccion;
+        #endregion
+
+        public void IniciarSistemaInfracciones(double baseMonetaria)
+        {
+            this.baseMonetaria=baseMonetaria;
+        }
+
+        public void IniciarActa(int dni, string nombre, int tipoVehiculo)
+        {
+            this.dniActa = dni;
+            this.nombreActa = nombre;
+            this.tipoVehiculo = tipoVehiculo;
+            subTotalActa = 0;
+        }
+
+        public void AgregarInfraccion(int tipoInfraccion)
+        {
+            codigoInfraccion = tipoInfraccion;
+            montoInfraccion = 0;
+            switch (tipoInfraccion)
+            {
+                case 1:
+                    {                        
+                        descripcionInfraccion = TIPO_1_DESCRP;
+                        unidadesInfraccion = TIPO_1_MONTO;                        
+                    }
+                    break;
+                case 2:
+                    {
+                        descripcionInfraccion = TIPO_2_DESCRP;
+                        unidadesInfraccion = TIPO_2_MONTO;
+                    }
+                    break;
+                case 3:
+                    {
+                        descripcionInfraccion = TIPO_3_DESCRP;
+                        unidadesInfraccion = TIPO_3_MONTO;
+                    }
+                    break;
+                case 4:
+                    {
+                        descripcionInfraccion = TIPO_4_DESCRP;
+                        unidadesInfraccion = TIPO_4_MONTO;
+                    }
+                    break;
+                case 5:
+                    {
+                        descripcionInfraccion = TIPO_5_DESCRP;
+                        unidadesInfraccion = TIPO_5_MONTO;
+                    }
+                    break;
+            }
+
+            montoInfraccion = unidadesInfraccion *baseMonetaria;
+            subTotalActa += montoInfraccion;
+        }
+
+        public void FinalizarActa(bool pagaEnElLugar)
+        {
+            double rec = 0;
+            switch (tipoVehiculo)
+            {
+                case 1:
+                    {
+                        rec = 1;
+                    } 
+                    break;
+                case 2:
+                    {
+                        rec = 50;
+                    }
+                    break;
+                case 3:
+                    {
+                        rec = 200;
+                    }
+                    break;
+            }
+            ajusteTipoVehiculo= subTotalActa * rec / 100;
+            subTotalAjustadoActa = subTotalActa + ajusteTipoVehiculo;
+
+            double desc = 0;
+            if (pagaEnElLugar)
+                desc = 50;
+            descuentoPagoActa = subTotalAjustadoActa *  desc/100;
+
+            totalActaAPagar = subTotalAjustadoActa -descuentoPagoActa;
+
+            if (pagaEnElLugar)
+            {
+                recaudacion += totalActaAPagar;
+            }
+        }
+    }
+}
